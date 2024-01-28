2590eff61f5ddb58ea57e2bd7e346d63dba49c20
Fatih <fatiharahmat257@gmail.com>
2024-01-28 16:24:49 +0700

    Initial Commit

---
+++ Sesi4Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sesi4Exercise
+//
+//  Created by Fatiha Rahmat on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sesi4Exercise/Sesi4ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesi4ExerciseApp.swift
+//  Sesi4Exercise
+//
+//  Created by Fatiha Rahmat on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesi4ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
